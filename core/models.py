cc3f0f31df2c160c4501951b8b96a932021a5514
VarunKumarGampa <gampavarunkumar@gmail.com>
2024-10-14 17:01:29 +0100

    user signup setup

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ User = get_user_model()
 # Create your models here.
 class Profile(models.Model):
   user = models.ForeignKey(User , on_delete=models.CASCADE)
-  id_user = models.IntegerField
+  id_user = models.IntegerField()
   bio = models.TextField(blank=True);
   profile_image = models.ImageField(upload_to= 'profile_image', default='blank-profile-picture.png')
   location = models.CharField(max_length=100, blank=True)
