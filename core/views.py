661e582339bebc69d4de02a6b120732e874eb907
VarunKumarGampa <gampavarunkumar@gmail.com>
2024-10-14 19:50:01 +0100

    created funtionalities like signin and logout

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 # Create your views here.
@@ -36,6 +37,30 @@ def signup(request):
     messages.info(request, 'Password Not Matching!!!')
     return redirect('signup')
 
+def signin(request):
 
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+
+      auth.login(request, user)  
+      return redirect('/')
+    else:
+      messages.info(request, 'Credentials Invalid')
+      return redirect('/signin')
+    
+  else:
+    return render(request, 'signin.html')
+
+@ login_required(login_url='signin')
+def logout(request):
+  auth.logout(request)
+  return redirect("/signin")
+
+@ login_required(login_url='signin')
 def index(request):
   return render(request, 'index.html')
